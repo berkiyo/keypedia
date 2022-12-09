0c82eb98332ac57d5e4c150782c8a960d6fcfb97
Berk Dogan <berk@tekbyte.net>
2022-12-09 21:21:17 +1100

    Add base project.

---
+++ Keypedia/KeypediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeypediaApp.swift
+//  Keypedia
+//
+//  Created by Berk Dogan on 9/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct KeypediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
